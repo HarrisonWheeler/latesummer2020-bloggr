b9b436e82f2f0f037b0798a114d9b8f6f3429f0d
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-07-31 16:59:33 -0600

    small changes

--- src/components/BlogModal.vue
+++ src/components/BlogModal.vue
@@ -3,13 +3,14 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
+          <h5 class="modal-title">Create a Blog Post!</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="modal-body">
-          <p>Modal body text goes here.</p>
+        <div class="input-group">
+          <h3>Title:</h3>
+          <input type="text" name id />
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-primary">Post Blog</button>
